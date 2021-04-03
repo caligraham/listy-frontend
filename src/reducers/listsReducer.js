b39fbb66f6f5448a33117808922ef1886c036804
Rebecca <graham.cali@gmail.com>
2021-04-03 14:39:57 -0500

    initial commit, added reducer

---
+++ src/reducers/listsReducer.js
@@ -0,0 +1,13 @@
+const initialState = {
+    lists: [],
+    loading: true
+}
+
+const listsReducer = (state, action) => {
+    switch(action.type) {
+        default: 
+        return state;
+    }
+}
+
+export default listsReducer
