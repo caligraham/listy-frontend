80bbffc8777d5aaded6f06230c5e14e40e008ebf
Rebecca <graham.cali@gmail.com>
2021-04-10 20:52:33 -0500

    image alignment

--- src/components/Happy.js
+++ src/components/Happy.js
@@ -4,19 +4,40 @@ import Mouse from './Images/Mouse.jpeg'
 import Quokka from './Images/Quokka.jpeg'
 import Puppy from './Images/Puppy.jpeg'
 import Sloth from './Images/Sloth.jpeg'
+import Image from 'material-ui-image';
+import { makeStyles } from '@material-ui/core/styles';
+
+
+const useStyles = makeStyles((theme) => ({
+    pic: {
+        marginTop: theme.spacing(15),
+        marginBottom: theme.spacing(15),
+        marginRight: theme.spacing(10),
+        marginLeft: theme.spacing(10)
+    }
+    }
+  ));
+
 
 const HappyAnimals = [Cat, Mouse, Quokka, Puppy, Sloth]
 
-class Happy extends Component {
-    render() {
-        
+
+
+
+const Happy=() => {
+   const classes = useStyles()
+     
         return (
 
-            <div>
-             <img className="images" src={HappyAnimals[Math.floor(Math.random() * 5)]}/>
+            <div className={classes.pic}>
+            <Image
+            src={HappyAnimals[Math.floor(Math.random() * 5)]}
+            aspectRatio={(16/9)}
+            />
+            {/* <img className="images" src={HappyAnimals[Math.floor(Math.random() * 5)]}/> */}
             </div>
         )
     }
-}
+
 
 export default Happy
