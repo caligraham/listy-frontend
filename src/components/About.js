9a1b0ec037aa890b6338ea2157f5a5fd0beaacce
Rebecca <graham.cali@gmail.com>
2021-04-10 20:52:00 -0500

    about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react'
 
+
 class About extends Component {
     render() {
         return (
             <div>
-                About this website 
-                <p>This is my website, learn all about it here!</p>
+            <h2>Welcome to Listy!</h2>
+            <hr></hr>
+            <br></br>
+            <br></br>
+            <br></br>
+              <p>
+                  Listy is an application to keep you organized and brighten your day. 
+                  All of your items are housed under the "List" tab. Create and delete your chores or errands as needed. 
+                  Feeling a little down, or just need a reason to smile? Mosey on over to the "Click here if sad tab" and we'll do our best to turn that frown upside down!
+
+
+                  Thank you for choosing Listy for all your productivity needs!</p> 
+                  <br></br>
+                  <br></br>
+                  <br></br>
+                  <hr></hr>
             </div>
         )
     }
