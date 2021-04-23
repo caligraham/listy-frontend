8c4f43aa0d9262ed485dce820fb4e8c51a3dca5c
Rebecca <graham.cali@gmail.com>
2021-04-23 13:53:48 -0500

    adding like incrementer

--- src/components/Index.js
+++ src/components/Index.js
@@ -3,15 +3,24 @@ import { connect } from 'react-redux';
 import ListItem from './ListItem';
 
 
-
-
 class Index extends Component {
+
+    state = {
+        input: 0
+    }
+
 
 
     formatDate = (date) => {
         const dueDate = new Date(date)
         let formattedDate = dueDate.toUTCString()
         return formattedDate.slice(0, -12)
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
     }
 
   
@@ -26,7 +35,10 @@ class Index extends Component {
         return (
             <div>
                 <br></br>
-                { this.props.lists && this.props.lists.map( (list, i) =>  <ListItem key={i} title={ list.title } due_date={ this.formatDate(list.due_date)} notes={ list.notes } id={ list.id } history={ this.props.history }/>) }
+                💗 incrementer 
+                <br/>
+                <input type="number" name="input" value={this.state.input} onChange={this.handleChange}></input>
+                { this.props.lists.map( (list, i) =>  <ListItem key={i} title={ list.title } due_date={ this.formatDate(list.due_date)} notes={ list.notes } id={ list.id } history={ this.props.history } input={this.state.input}/>) }
                 <br></br>
             </div>
         )
