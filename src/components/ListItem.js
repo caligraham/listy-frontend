e33dae8c261e4a232df56dae437a1443828694aa
Rebecca <graham.cali@gmail.com>
2021-04-23 13:53:40 -0500

    adding like button

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -6,12 +6,17 @@ import { deleteList } from '../actions/index'
 
 class ListItem extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            count: 0
+        };
+    }
 
 
     render() {
-        const { title, due_date , notes, id } = this.props;
+        const { title, due_date , notes, id, input } = this.props;
         return (
-    
             <div>
                 <br></br>
                 <div className="container">
@@ -20,13 +25,18 @@ class ListItem extends Component {
                 <h6>{due_date }</h6>
                 <p>{ notes }</p>
                 <br />
-                <button className="button-color" onClick={() => this.props.deleteList(id, this.props.history)}>Just did it 🦄</button>
+                <button className="button-color" onClick={() => this.props.deleteList(id, this.props.history)}>Just did it 🦄</button> 
+                <p> {this.state.count} likes</p> 
+                <button onClick={() => this.setState({ count: this.state.count + parseInt(input)})}>💗</button>
                 </div>
             </div>
         )
     }
 
 }
+// history is prop provided by react router
 
 export default connect(null, {deleteList})(ListItem)
+
+// dispatcher action 
 
